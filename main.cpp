b53b5adaa8cf4ca3cb11abf7a079cd359ad2f47f
BladeofDagger <43705981+BladeofDagger@users.noreply.github.com>
2018-12-02 18:34:21 +0100

    zlicza ilosc par znakow

--- main.cpp
+++ main.cpp
@@ -1,40 +1,53 @@
 //
 //  main.cpp
-//  c++pointers_ex6list2
+//  ex_11list_1_zlicza ilosc sasiadujacych par znakow
 //
-//  Created by Martyna Gruszka on 26.11.2018.
+//  Created by Martyna Gruszka on 02.12.2018.
 //  Copyright © 2018 Martyna Gruszka. All rights reserved.
-//
+//Napisz program, który zlicza ilość wystąpień par sąsiadujących ze sobą liter ’o’ w
+//podanym przez użytkownika ciągu.
 
 #include <iostream>
 using namespace std;
 
-void convert(char *a, char *b);
-char *a = nullptr;
-char *b = nullptr;
-char text[1024]="Pogoda";
-char convertedText[1024]{};
+char ciag_znakow[255]{};
+char znak{};
+void ile_razy_wystapil_znak(char *c,char z);
 
 int main() {
     
-    convert(text,convertedText);
-    cout << text << endl;
-    cout << convertedText << endl;
+    cout << "Wprowadz swoj ciag znakow: ";
+    cin.getline(ciag_znakow,255);
+    cout<<endl;
+    cout<< "Wprowadz znak: ";
+    cin >> znak;
+    ile_razy_wystapil_znak(ciag_znakow,znak);
+
     
-    system("PAUSE");
     
+}
+void ile_razy_wystapil_znak(char *c,char z){
+    
+
+
+    int i=0;
+    int ilosc_par=0;
+    
+
+    while (*c != 0){
+        
+        
+        
+                if (*(c+i)==z && *(c+i+1)==z){
+                    ilosc_par++;
+                    i++; // sprawdzam pary
+                }
+                
+        
+        
+        c++;
+    }
+    
+    cout << " Para znaków " << "'" << z <<"'" << " wystąpił " << ilosc_par << " razy " << endl;
 }
 
-void convert(char *a, char *b){
-    int i = 0;
-    while (*a != 0){
-        i++;
-        a++;
-        
-    }
-    for (int j=0; j<i; j++){
-        *b++=*(--a);
-        *b = 0;
-    }
-    
-}
